271a06d5cafa671826890ce37b881e6d32d27743
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 21:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function firstChar(text) {
   // your code here
-	return text.trim()[0];
+	if(text.trim() == ''){
+		return '';
+	}
+	else{
+		return text.trim()[0];
+		
+	}
 }
 
 // Do not change the code below
-//Uncomment the following line to showvv the prompt popup
-const text = prompt("Enter text:");
-alert(firstChar(text));
+//Uncomment the following line to shkiowvv the prompt popup
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
